633eb7d4f313532dab9d9eefc44348ad2f5d5356
Impulse2020 <irishht@gmail.com>
2020-06-08 17:19:07 -0400

    changed the reducer

--- todo/reducers/reducer.js
+++ todo/reducers/reducer.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const initialState = {
-    todo:'learn about reducers',
+    todo: 'learn about reducers',
     done: false,
     id: 3892987589
 }
-const reducer = (state, action) => {
- return state;
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case "complete":
+            return {
+                ...state,
+                done: !state.done
+            }
+        case "incomplete":
+            return {
+                ...state,
+                done: false
+            }
+         default:
+             return state;
+    }
 
 }
-
-export default {initialState, reducer};
